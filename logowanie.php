14332867db8fdd741e2daad623fa253596e8aca4
Fiedor <stachfie@wp.pl>
2024-01-13 14:51:33 +0100

    PHP done

--- logowanie.php
+++ logowanie.php
@@ -24,7 +24,32 @@
                 <input type="submit" value="Zapisz">
             </form>
             <?php
-            //s1
+            $conn = mysqli_connect('localhost', 'root', '', 'psy');
+            if (empty($_POST['login']) || empty($_POST['haslo']) || empty($_POST['haslo1'])) {
+                echo "<p>Nie wypełniono przynajmniej jednego pola formularza</p>";
+            } else {
+                $l = $_POST['login'];
+                $h = $_POST['haslo'];
+                $h1 = $_POST['haslo1'];
+                $res = mysqli_query($conn, "SELECT login FROM uzytkownicy;");
+                foreach ($res as $r) {
+                    if ($r['login'] == $l) {
+                        echo "<p>Login występuje w bazie danych hasło nie zostało dodane</p>";
+                        break;
+                    } else {
+                        $pop = true;
+                    }
+                }
+                if ($h != $h1) {
+                    echo "<p>Hasła nie są takie same, konto nie zostało dodane</p>";
+                    $pop = false;
+                }
+                if ($pop == true) {
+                    $h = sha1($h);
+                    mysqli_query($conn, "INSERT INTO `uzytkownicy`( `login`, `haslo`) VALUES ('$l','$h');");
+                }
+            }
+            mysqli_close($conn);
             ?>
         </div>
         <div id="prawy2">
